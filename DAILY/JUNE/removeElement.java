a6cb9689e760a328645a551c677d53fde884ceac
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-26 23:59:50 +0530

    remove elements

---
+++ DAILY/JUNE/removeElement.java
@@ -0,0 +1,21 @@
+public class removeElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        int newLength = removeElement(nums, val);
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
