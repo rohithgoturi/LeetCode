727d9cf1bd2a422e8d8a52d59c7edd682b8b5c0e
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-27 01:08:02 +0530

    first and last occurrance of an element

---
+++ DAILY/JUNE/firstAndLast.java
@@ -0,0 +1,47 @@
+public class firstAndLast {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] result = searchRange(nums, target);
+        System.out.println("First and Last Position of Target: [" + result[0] + ", " + result[1] + "]");
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid;
+                right = mid - 1; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (result[0] == -1) {
+            return result;
+        }
+
+
+        left = result[0];
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[1] = mid;
+                left = mid + 1; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
