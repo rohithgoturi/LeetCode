19704164e60fc022694b123c7a9fd620101ab749
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-06-29 23:53:53 +0530

    arranging coins using optimal approach

--- DAILY/JUNE/arrangingCoins.java
+++ DAILY/JUNE/arrangingCoins.java
@@ -1,23 +1,20 @@
 public class arrangingCoins {
     // leetcode 441
-    public static int sqrt(long n) {
-        long low = 1;
-        long high = n;
+    
+    public static int arrangeCoins(int n) {
+        int low = 0;
+        int high = n;
 
         while(low <= high){
             long mid = low + (high - low)/2;
+            long coins = mid * (mid + 1) / 2;
 
-            if(mid == n/mid) return (int) mid;
-            else if(mid > n/mid) high = mid-1;
-            else low = mid+1;
+            if(coins == n) return (int)mid;
+            else if(coins < n) low = (int)mid + 1;
+            else high = (int)mid - 1;
         }
 
-        return (int) high;
-    }
-    
-    public static int arrangeCoins(int n) {
-        long m = (long) n;
-        return (sqrt(8*m+1) - 1)/2;
+        return high;
     }
 
     public static void main(String[] args) {
