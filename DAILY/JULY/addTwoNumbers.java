047a497e56cf5c1f8e02810f30bc4f1535923c7e
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-02 03:09:52 +0530

    adding two numbers (easiest leetcode question

---
+++ DAILY/JULY/addTwoNumbers.java
@@ -0,0 +1,12 @@
+public class addTwoNumbers {
+    public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int result = sum(num1, num2);
+        System.out.println("Sum: " + result);
+    }
+}
