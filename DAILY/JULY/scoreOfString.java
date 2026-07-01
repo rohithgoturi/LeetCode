4e30b0994588d452441cf48ec5dcafc9e9519c4f
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-02 03:18:34 +0530

    finding score of string using ascii values

---
+++ DAILY/JULY/scoreOfString.java
@@ -0,0 +1,20 @@
+public class scoreOfString {
+     public static int scoreOfString(String str) {
+        int n = str.length();
+        int score = 0;
+        for(int i = 0; i<n-1; i++){
+            int cur = (int) str.charAt(i) - (int) str.charAt(i+1);
+            if(cur<0) cur = -cur;
+            score += cur;
+        }
+
+        return score;
+    }
+
+    public static void main(String[] args) {
+        String str = "hello";
+        int result = scoreOfString(str);
+        System.out.println("Score of the string: " + result); 
+    }
+
+}
