cce07898e3ed5ca99d823799cec699c65a272595
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-02 03:07:18 +0530

    reversing string using two pointers technique

---
+++ DAILY/JULY/reverseString.java
@@ -0,0 +1,22 @@
+public class reverseString {
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+
+        System.out.println("Reversed array: " + new String(s));
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        System.out.println("Original array: " + new String(s));
+        reverseString(s);
+    }
+}
