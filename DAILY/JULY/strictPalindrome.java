099a036601c44898a0796b7832a83065e3472748
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-02 21:14:43 +0530

    strict palindrome is not possible for given constraints so returning false

---
+++ DAILY/JULY/strictPalindrome.java
@@ -0,0 +1,12 @@
+public class strictPalindrome {
+    public static boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        boolean result = isStrictlyPalindromic(n);
+        System.out.println("Is strictly palindromic: " + result);
+    }
+
+}
