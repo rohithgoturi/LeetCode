0962b5ee1fb7a937029bbeefae3443c872b4beb7
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-04 21:59:36 +0530

    finding middle node of a linked list using slow and fast approach

---
+++ DAILY/JULY/middleOfLinkedList.java
@@ -0,0 +1,41 @@
+class Node{
+    int data;
+    Node next;
+
+    Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class middleOfLinkedList {
+    public static Node middleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(5);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        Node middle = middleNode(a);
+        System.out.println("Middle node data: " + middle.data);
+    }
+
+
+}
