6f4bd0882179f833dfbcc3c28770d036c69fdca6
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-05 18:01:18 +0530

    swapping nodes of linkedlist: nth from start with nth from last

---
+++ DAILY/JULY/swapNodes.java
@@ -0,0 +1,41 @@
+public class swapNodes {
+    public static Node swapNodes(Node head, int k) {
+        Node fast = head;
+        Node slow = head;
+
+        for(int i=1; i<k; i++){
+            fast = fast.next;
+        }
+
+        Node starting = fast;
+
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        int temp = starting.data;
+        starting.data = slow.data;
+        slow.data = temp;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        int k = 2;
+        head = swapNodes(head, k);
+
+        // Print the modified linked list
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
