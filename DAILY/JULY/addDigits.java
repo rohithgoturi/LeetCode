34150dbd45f3524a6efab4a083e68ac11c1dcf12
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-08 22:59:38 +0530

    addDigits : constructor warning fixed

--- DAILY/JULY/addDigits.java
+++ DAILY/JULY/addDigits.java
@@ -1,5 +1,5 @@
 public class addDigits {
-    public static int addDigits(int num){
+    public static int addingDigits(int num){
         if(num == 0) return 0;
 
         int ans = 0;
@@ -9,12 +9,12 @@ public class addDigits {
         }
 
         if(ans <= 9) return ans;
-        else return addDigits(ans);
+        else return addingDigits(ans);
     }
 
     public static void main(String[] args) {
         int num = 38;
-        int result = addDigits(num);
+        int result = addingDigits(num);
         System.out.println("Add digits result: " + result);
     }
 
