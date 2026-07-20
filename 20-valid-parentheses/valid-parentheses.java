a379f545e902652c52cc868f9353f401bee0b863
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-20 23:50:41 +0530

    Time: 3 ms (86.86%) | Memory: 43 MB (86.08%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String str) {
+        int n = str.length();
+        if(n%2 != 0) return false;
+        
+        Stack<Character> st = new Stack<>();
+        
+        for(int i=0; i<n; i++){
+            char ch = str.charAt(i);
+            
+            if(ch =='(' || ch == '{' || ch == '[') st.push(ch);
+            else{
+                if(st.isEmpty()) return false;
+                
+                char top = st.peek();
+                if(sameStyle(top, ch)) st.pop();
+                else return false;
+            }
+        }
+        
+        return st.isEmpty();
+    }
+    
+    public boolean sameStyle(char a, char b){
+        if(a == '(' && b == ')') return true;
+        if(a == '[' && b ==']') return true;
+        if(a == '{' && b == '}') return true;
+        
+        return false;
+    }
+}
