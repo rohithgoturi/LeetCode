bc4ed69a8b7288b8b6c612ac40e4d4fbf58f32fe
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-22 23:21:16 +0530

    Time: 3 ms (4.21%) | Memory: 42.6 MB (69.48%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,41 +1,26 @@
 class MyQueue {
-    Stack <Integer> st = new Stack<>();
+    Stack<Integer> st = new Stack<>();
     Stack<Integer> helper = new Stack<>();
-
     public MyQueue() {
         
     }
-    
-    public void push(int x) {
+
+    public void push(int x) { 
+        while(st.size()>0){
+            helper.push(st.pop());
+        }
         st.push(x);
+        while(helper.size()>0){
+            st.push(helper.pop());
+        }
     }
     
     public int pop() {
-        while(st.size()>1){
-            helper.push(st.pop());
-        }
-
-        int front = st.pop();
-
-        while(helper.size()>0){
-            st.push(helper.pop());
-        }
-
-        return front;
+        return st.pop();
     }
     
-    public int peek() {
-        while(st.size()>1){
-            helper.push(st.pop());
-        }
-
-        int front = st.peek();
-
-        while(helper.size()>0){
-            st.push(helper.pop());
-        }
-
-        return front;
+    public int peek() { 
+        return st.peek();
     }
     
     public boolean empty() {
