0a702f5cd7cd39e7843c55c99e73c78894f2bfec
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2026-07-23 23:57:41 +0530

    Time: 1 ms (65.86%) | Memory: 43.1 MB (14.39%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -3,29 +3,21 @@ class MyStack {
     public MyStack() {
         
     }
-
-
     
-    public void push(int x) {
+       public void push(int x) {
         q.add(x);
+        int n = q.size();
+        for(int i=1;i<=n-1;i++){
+            q.add(q.remove());
+        }
     }
     
     public int pop() { 
-        int n = q.size();
-        for(int i=1;i<=n-1;i++){
-            q.add(q.remove());
-        }
         return q.remove();
     }
     
-    public int top() { 
-        int n = q.size();
-        for(int i=1;i<=n-1;i++){
-            q.add(q.remove());
-        }
-        int p = q.peek();
-        q.add(q.remove()); 
-        return p;
+    public int top() {
+        return q.peek();
     }
     
     public boolean empty() {
